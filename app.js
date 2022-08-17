bd813ee6375a042f2a9e46d7524cfad7e2c99c52
azizul-orvee <orvee16@gmail.com>
2022-08-18 02:55:50 +0600

    added

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import router from "./routes/user-routes";
+
+
+const app = express();
+app.use(express.json())
+
+app.use('/api/user', router)
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:1234@cluster0.tqhmowx.mongodb.net/Blog?retryWrites=true&w=majority"
+  )
+  .then(() => app.listen(5000))
+  .then(()=> console.log("Connected DB >>> <<<"))
+  .catch((err) => console.log(err));
 
 
