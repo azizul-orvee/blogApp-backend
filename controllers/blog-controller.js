850fd39f99a76dfe88c0e84881cf239f918f87ae
azizul-orvee <orvee16@gmail.com>
2022-08-18 14:21:14 +0600

    added

--- controllers/blog-controller.js
+++ controllers/blog-controller.js
@@ -34,15 +34,47 @@ export const udpateBlog = async (req, res, next) => {
   const blogId = req.params.id;
   let blog;
   try {
-     blog = await Blog.findByIdAndUpdate(blogId, {
+    blog = await Blog.findByIdAndUpdate(blogId, {
       title,
       description,
     });
+    blog = await Blog.findById(blogId)
   } catch (error) {
     return console.log(error);
   }
   if (!blog) {
     return res.status(500).json({ message: "Unable to update Blog" });
-  }
+  } 
   return res.status(200).json({ blog });
+  
 };
+
+export const getById = async (req,res,next) => {
+  const id = req.params.id;
+  let blog;
+  try {
+    blog = await Blog.findById(id)
+  } catch (error) {
+     console.log(error);
+     return res.status(404).json({message: "Blog not found"})
+  }
+  if(!blog){
+    return res.status(404).json({message: "No blog found"})
+  }
+  return res.status(200).json({blog})
+}
+
+export const deleteBlog = async(req,res,next) => {
+  const id = req.params.id;
+  let blog;
+  try {
+    blog = await Blog.findByIdAndRemove(id)
+  } catch (error) {
+    console.log(error)
+    return res.status(404).json({message: "No blog found"})
+  }
+  if(!blog){
+    return res.status(500).json({message: "No blog found"})
+  }
+  return res.status(200).json({message: "Successfully Deleted"})
+}
--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { addBlog, getAllBlogs, udpateBlog } from "../controllers/blog-controller";
+import { addBlog, deleteBlog, getAllBlogs, getById, udpateBlog } from "../controllers/blog-controller";
 
 const blogRouter = express.Router();
 
 blogRouter.get("/", getAllBlogs);
 blogRouter.post('/add', addBlog)
 blogRouter.put("/update/:id",udpateBlog)
+blogRouter.get("/:id",getById)
+blogRouter.delete("/:id",deleteBlog)
 
 
 export default blogRouter;
